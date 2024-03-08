3f851a95983334bc477673a5f9bc804939593faf
Reberofreddy <reberofreddy2@gmail.com>
2024-03-08 02:58:01 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,11 @@
   *@: the number to calculate the factorial
   *Return: integer value
   */
-int factorial(int i)
+int factorial(int n)
 {
-if(i < 0)
+if(n < 0)
 return (-1);
-if (i <= 1)
+if (n <= 1)
 return (1);
-return (i * factorial(i - 1));
+return (n * factorial(n - 1));
 }
