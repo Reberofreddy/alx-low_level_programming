51f123172ad0ba193371857ce9d13fe1e968c780
Reberofreddy <reberofreddy2@gmail.com>
2024-03-08 02:46:35 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+  *factorial - Write a function that returns the factorial
+  *@: the number to calculate the factorial
+  *Return: integer value
+  */
+int factorial(int i)
+{
+if(i < 0)
+return (-1);
+if (n <= 1)
+return (1);
+return (n * factorial(n - 1));
+}
